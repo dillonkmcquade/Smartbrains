9f9b66f348975a07faac72a5d098ff6af368efec
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-17 15:55:42 -0500

    added footer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { connect } from "react-redux";
-import Navigation from '../components/navigation/Navigation';
-import { selectIsLoggedIn, selectIsLoading } from "../Redux/user/user.selectors";
+import Navigation from "../components/navigation/Navigation";
+import {
+  selectIsLoggedIn,
+  selectIsLoading
+} from "../Redux/user/user.selectors";
 import HomePage from "../pages/homepage/homepage.component";
 import SignIn from "../components/Signin/Signin";
 import Register from "../components/Register/Register";
 import { Switch, Route } from "react-router-dom";
 import RingLoaderComponent from "../components/ringloader";
+import Footer from '../components/footer/footer.component';
 
-const App = ({ isLoggedIn , isLoading}) => {
+const App = ({ isLoggedIn, isLoading }) => {
   return (
     <div className="App">
       <Navigation />
       {!isLoggedIn ? (
         <Switch>
-          <Route exact path="/" component={SignIn} />
+          <Route
+            exact
+            path="/"
+            component={isLoading ? RingLoaderComponent : SignIn}
+          />
           <Route path="/register" component={Register} />
         </Switch>
       ) : (
-        isLoading ? <RingLoaderComponent /> : <HomePage />
+        <HomePage />
       )}
+      <Footer />
     </div>
   );
 };
