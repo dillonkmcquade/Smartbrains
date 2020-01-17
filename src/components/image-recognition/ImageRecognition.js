40654daafa9db67e7c61bc9a71657b7280ba9a6b
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-17 09:52:00 -0500

    mobile css changes to image recognition component

--- src/components/image-recognition/ImageRecognition.js
+++ src/components/image-recognition/ImageRecognition.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { selectImageUrl } from "../../Redux/food/food.selectors";
 import { connect } from "react-redux";
-import "./image-recognition.css";
+import "./image-recognition.styles.scss";
 
 const ImageRecognition = ({ imageURL }) => {
   return (
