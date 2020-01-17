b93223531b800f77a5d6bcf4a45bfa807c4349fe
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-17 16:59:06 -0500

    fixed ingredient-item component

--- src/components/ingredient-item/ingredient-item.component.jsx
+++ src/components/ingredient-item/ingredient-item.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "tachyons";
-import './ingredient-item.styles.scss';
+import "./ingredient-item.styles.scss";
 
 const IngredientItem = ({ name, value }) => {
   const probability = Math.floor(value * 100);
   return (
     <div className="ingredient-item">
       <div className="ingredient-name">{name}</div>
-      <div className="ingredient-value">{value == 'Probability' ? value : probability + '%'}</div>
+      <div className="ingredient-value">
+        {value === "Probability" ? value : probability + "%"}
+      </div>
     </div>
   );
 };
