16329f5b8f55019a3179141414374b6867704df1
Dillon <dillonkmcquade@gmail.com>
2020-03-08 09:21:02 -0400

    css changes to register and signin component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -33,65 +33,67 @@ const Register = ({ registerUser, setLoading, isLoading }) => {
       .catch(console.log("error registering user"));
   };
   if (isLoading) {
-    return <RingLoaderComponent />
+    return <RingLoaderComponent />;
   } else {
     return (
-      <article className="mw6 register-component center bg-white br3 pa3 pa4-ns mv3 ba shadow-5 bw3 b--light-green">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className=" fw6 ph0 f2 mh0">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">
-                  Name
-                </label>
+      <div className='register-container'>
+        <article className="mw6 register-component center bg-white br3 pa3 pa4-ns mv3 ba shadow-5 bw3 b--light-green">
+          <main className="pa4 black-80">
+            <div className="measure">
+              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className=" fw6 ph0 f2 mh0">Register</legend>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f6" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    onChange={event => setName(event.target.value)}
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="text"
+                    name="name"
+                    id="name"
+                    required
+                  />
+                </div>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                    Email
+                  </label>
+                  <input
+                    onChange={event => setEmail(event.target.value)}
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    required
+                  />
+                </div>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f6" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    onChange={event => setPassword(event.target.value)}
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="password"
+                    name="password"
+                    id="password"
+                    required
+                  />
+                </div>
+              </fieldset>
+              <div>
                 <input
-                  onChange={event => setName(event.target.value)}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
-                  required
+                  onClick={onSubmitRegister}
+                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                  type="submit"
+                  value="Register"
                 />
               </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
-                <input
-                  onChange={event => setEmail(event.target.value)}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  required
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  onChange={event => setPassword(event.target.value)}
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  required
-                />
-              </div>
-            </fieldset>
-            <div>
-              <input
-                onClick={onSubmitRegister}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Register"
-              />
             </div>
-          </div>
-        </main>
-      </article>
+          </main>
+        </article>
+      </div>
     );
   }
 };
